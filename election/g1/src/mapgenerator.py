2eac41eb4b745ae50f634e900ac13ec1f9e2c928
Adam Svystun <adam.svystun@gmail.com>
2019-10-01 17:42:48 -0400

    Add typings

--- election/g1/src/mapgenerator.py
+++ election/g1/src/mapgenerator.py
@@ -1,23 +1,28 @@
-def get_normal(num_voters, mean, sigma, seed):
+from typing import List
+
+from election.g1.src.voter import Voter
+
+
+def get_normal(num_voters: int, mean, sigma, seed: int) -> List[Voter]:
     #@TODO Derek
     pass
 
 
-def get_uniform(num_voters, seed):
+def get_uniform(num_voters: int, seed: int) -> List[Voter]:
     #@TODO Derek
     pass
 
 
-def get_party_preference(voters, seed):
+def get_party_preference(voters: List[Voter], seed: int) -> List[Voter]:
     #@TODO Patrick
     pass
 
 
-def get_coast(num_voters, seed):
+def get_coast(num_voters: int, seed: int) -> List[Voter]:
     #@TODO Patrick
     pass
 
 
-def get_voters(num_voters, num_parties, seed):
-    pass
+def get_voters(num_voters: int, num_parties: int, seed: int) -> List[Voter]:
+    return []
 
--- election/g1/srcipts/generatemap.py
+++ election/g1/srcipts/generatemap.py
@@ -2,19 +2,26 @@ import argparse
 import importlib
 
 
-def save_map_to_file(filepath, voters, districts):
-    pass
+def save_map_to_file(filepath, voters, districts, parties):
+    file = open(filepath, 'w')
+    file.write(f'{str(len(voters))} {str(parties)}')
+    for voter in voters:
+        file.write(voter.get_file_line())
+    file.write(f'{str(len(districts))}')
+    for district in districts:
+        file.write(district.get_file_line())
+    file.close()
 
 
 def main(options):
-    generator_module = importlib.import_module(options['class'])
-    voters = generator_module.get_voters(options['voters'], options['parties'], options['seed'])
-    save_map_to_file(options['output'], voters, [])
+    generator_module = importlib.import_module(options.module)
+    voters = generator_module.get_voters(options.voters, options.parties, options.seed)
+    save_map_to_file(options.output, voters, [], options.parties)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--class', '-c', type=str, default="election.g1.src.mapgenerator")
+    parser.add_argument('--module', '-m', type=str, default="election.g1.src.mapgenerator")
     parser.add_argument('--output', '-o', type=str, default="maps/g1/1.map")
     parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--voters', type=int, default=333333)
