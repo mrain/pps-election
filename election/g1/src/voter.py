a0f38753fa2f1a9f15d788e5259e3be377d082c0
Adam Svystun <adam.svystun@gmail.com>
2019-10-01 17:48:16 -0400

    Add point representation

--- election/g1/src/voter.py
+++ election/g1/src/voter.py
@@ -1,5 +1,15 @@
+from typing import Optional, List
+
+
+class Point:
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+
 class Voter:
 
-    def __init__(self, location=None, preference=None):
+    def __init__(self, location: Optional[Point] = None, preference: Optional[List[float]] = None):
         self.location = location
         self.preference = preference
