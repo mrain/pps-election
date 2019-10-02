8dd6f918c742f0f4af278e99d13aa532fca0c62c
ethan dunn <ed2729@columbia.edu>
2019-10-02 00:16:31 -0400

    adding code and maps for first deliverable

---
+++ election/g5/CenterOfInfluence.java
@@ -0,0 +1,23 @@
+package election.g5;
+
+import java.awt.geom.*;
+
+public class CenterOfInfluence {
+
+	private Point2D location;
+	private List<Double> preferences;
+
+	public CenterOfInfluence(Point2D location, List<Double> preferences) {
+		this.location = location;
+		this.preferences = preferences;
+	}
+
+	public Point2D getLocation() {
+		return location;
+	}
+
+	public List<Double> getPreferences() {
+		return preferences;
+	}
+
+}
