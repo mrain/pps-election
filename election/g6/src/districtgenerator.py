c40a1037868bd3d78c9292fd965e32390b851cd4
Adam Svystun <adam.svystun@gmail.com>
2019-10-03 16:50:42 -0400

    Update district generation module

--- election/g6/src/districtgenerator.py
+++ election/g6/src/districtgenerator.py
@@ -5,6 +5,18 @@ from shapely.geometry import Polygon
 from election.g6.src.voter import Voter
 
 
-def get_districts(voters: List[Voter], representatives_per_district: int, seed: int) -> List[Polygon]:
+def get_initial_triangles(voters: List[Voter], n_triangles: int, population: int, seed: int) -> List[Polygon]:
     # TODO: Derek
     pass
+
+
+def get_districts_from_triangles(triangles: List[Polygon], n_districts: int, seed: int) -> List[Polygon]:
+    # TODO: Adam
+    pass
+
+
+def get_districts(voters: List[Voter], representatives_per_district: int, population: int, seed: int) -> List[Polygon]:
+    n_triangles = 81 * 7
+    triangles = get_initial_triangles(voters, n_triangles, population, seed)
+    districts = get_districts_from_triangles(triangles, 81, seed)
+    return districts
