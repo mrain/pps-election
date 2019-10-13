0dbb7586c542fd978b66280213a66f987f9532f3
Griffin Adams <griff4692@gmail.com>
2019-10-13 17:51:19 -0400

    Changes.

--- election/g8/voter.py
+++ election/g8/voter.py
@@ -6,6 +6,7 @@ from IPython import display
 import math
 import random
 from sklearn.cluster import KMeans
+from shapely.geometry import Point, Polygon, LineString
 from scipy.spatial import Voronoi, voronoi_plot_2d
 %matplotlib inline
 
@@ -56,6 +57,43 @@ def insideBoundary(voter):
 
 def area(x1, y1, x2, y2, x3, y3):
     return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0)
+
+def sample_new_point(prev_x, prev_y, area):
+    def out_of_bounds(x, y):
+        center = (500, 250 * np.sqrt(3))
+        top = (500, 500 * math.sqrt(3))
+        left = (0, 0)
+        right = (1000, 0)
+        line = LineString([(x, y), center])
+        e1 = LineString([left, right])
+        e2 = LineString([left, top])
+        e3 = LineString([right, top])
+        return line.intersects(e1) or line.intersects(e2) or line.intersects(e3)
+
+    mean = [0, 0]
+    cov = [[area, 0], [0, area]]
+    while True:
+        delta_x, delta_y = np.random.multivariate_normal(mean, cov, 1).T
+        delta_x = delta_x[0]
+        delta_y = delta_y[0]
+        new_x, new_y = prev_x + delta_x, prev_y + delta_y
+        if not out_of_bounds(new_x, new_y):
+            return new_x, new_y
+
+def adjust_voter_preference(pref, target_p2=0.5):
+    p1_boost = 0.5 - target_p2
+    p2_boost = target_p2 - 0.5
+    beta = max(0.01, p1_boost + pref[0])
+    alpha = max(0.01, p2_boost + pref[1])
+    p2_prob = np.random.beta(alpha, beta)
+    return [1.0 - p2_prob, p2_prob]
+
+def sample_vote(pref):
+    assert sum(pref) == 1.0
+    return np.random.binomial(size=1, n=1, p=pref[1])[0]
+
+print(sample_new_point(250, 250, 100))
+print(sample_vote(adjust_voter_preference([0.5, 0.5], target_p2=0.6)))
 
 voters = extractVoters("../../maps/g8/twoParties.map")
 
