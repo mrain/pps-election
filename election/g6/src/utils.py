924e31a6a7c5625e10806844015728c2434804e1
Adam Svystun <adam.svystun@gmail.com>
2019-10-14 18:48:27 -0400

    Add sorted population counting

--- election/g6/src/utils.py
+++ election/g6/src/utils.py
@@ -1,15 +1,71 @@
-import random
-from collections import defaultdict
-from itertools import combinations, product
 from typing import List, Tuple, Dict
 
-import metis
-import networkx as nx
 from shapely.geometry import Polygon
-from shapely.ops import cascaded_union
 
-from election.g6.src import dist_analysis
 from election.g6.src.voter import Voter
+
+
+def get_bounding_box(polygon: Polygon) -> Tuple[float, float, float, float]:
+    x_min, y_min, x_max, y_max = 1000., 1000., 0., 0.
+    x_coords, y_coords = polygon.exterior.coords.xy
+    for x in x_coords:
+        if x < x_min:
+            x_min = x
+        if x > x_max:
+            x_max = x
+    for y in y_coords:
+        if y < y_min:
+            y_min = y
+        if y > y_max:
+            y_max = y
+    return x_min, y_min, x_max, y_max
+
+
+def get_voters_in_range(voters: List[Voter], p_min, p_max, p_get) -> List[Voter]:
+    voters_in_range = []
+    for v in voters:
+        p = p_get(v)
+        if p > p_max:
+            break
+        if p >= p_min:
+            voters_in_range.append(v)
+    return voters_in_range
+
+
+def count_population_in_polygon(voters: List[Voter], polygon) -> int:
+    count = 0
+    for v in voters:
+        if polygon.contains(v.location):
+            count += 1
+    return count
+
+
+def get_population_in_polygons(voters: List[Voter], polygons: List[Polygon]) -> List[int]:
+    # Sort the population according to x into two array
+    voters_by_x = sorted(voters, key=lambda x: x.location.x)
+    # Count population for each polygon
+    population_counts = []
+    for index, polygon in enumerate(polygons):
+        # Find the bounding box for the polygon
+        x_min, y_min, x_max, y_max = get_bounding_box(polygon)
+        # Get voters in the given x
+        polygon_voters_by_x = get_voters_in_range(voters_by_x, x_min, x_max, lambda x: x.location.x)
+        # Sort them according to y
+        polygon_voters_by_y = sorted(polygon_voters_by_x, key=lambda x: x.location.y)
+        # Get voters in range of y
+        polygon_voters_in_box = get_voters_in_range(polygon_voters_by_y, y_min, y_max, lambda x: x.location.y)
+        # Count population in the polygon
+        population = count_population_in_polygon(polygon_voters_in_box, polygon)
+        population_counts.append(population)
+    return population_counts
+
+
+def get_population_in_polygons_basic(voters: List[Voter], polygons: List[Polygon]) -> List[int]:
+    population_counts = []
+    for index, polygon in enumerate(polygons):
+        population = count_population_in_polygon(voters, polygon)
+        population_counts.append(population)
+    return population_counts
 
 
 def batch(iterable, n=1):
